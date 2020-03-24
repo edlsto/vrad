f6de860b161d0c33fee0aa48632de534701c4583
Edward Stoner <edwardlstoner@gmail.com>
2020-03-24 14:23:02 -0600

    Set up Nav component

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,2 +1,14 @@
 import React from "react";
 import "./Nav.css";
+
+const Nav = () => {
+  return (
+    <nav>
+      <h1>
+        VRAD<span>Vacation Rentals Around Denver</span>
+      </h1>
+    </nav>
+  );
+};
+
+export default Nav;
