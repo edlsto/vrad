e43c4606c50ca9476ccdae8235f913d704a7806b
Dan Reardon <dreardon1021@gmail.com>
2020-03-24 20:28:26 -0600

    Setup Area functional component and styling

--- src/components/Area/Area.js
+++ src/components/Area/Area.js
@@ -1,2 +1,18 @@
 import React from "react";
 import "./Area.css";
+
+
+const Area = ({ about, id, shortName, longName, listings }) => {
+  return(
+    <article id={id}>
+      <div className="area-card-info">
+        <h3>{shortName}</h3>
+        <h4>{longName}</h4>
+        <p>{about}</p>
+      </div>
+      <button className="view-listings-button">View Listings</button>
+    </article>
+  )
+}
+
+export default Area
--- src/components/CardsContainer/CardsContainer.js
+++ src/components/CardsContainer/CardsContainer.js
@@ -1,12 +1,25 @@
 import React from "react";
 import "./CardsContainer.css";
+import Area from "../Area/Area";
 
-const CardsContainer = () => {
+const CardsContainer = ({ areas }) => {
   return(
     <section className="page-body">
       <h2>Please Select An Area</h2>
       <section className="card-container">
-      
+        {areas.map(area => {
+          return <Area
+            about={area.about}
+            id={area.id}
+            key={area.id}
+            shortName={area.area}
+            location={area.location}
+            longName={area.name}
+            quickSearch={area.quick_search}
+            regionCode={area.region_code}
+            listings={area.listings}
+          />
+        })}
       </section>
     </section>
   )
