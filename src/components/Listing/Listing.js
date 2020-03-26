e905f589e05cc85028117be84e5b62a6991e1a72
Edward Stoner <edwardlstoner@gmail.com>
2020-03-25 20:15:59 -0600

    Creating Listing component

--- src/components/Listing/Listing.js
+++ src/components/Listing/Listing.js
@@ -1,2 +1,13 @@
 import React from "react";
 import "./Listing.css";
+
+const Listing = props => {
+  return (
+    <div className="card">
+      <div>{props.name}</div>
+      <img src="../../../public/images/" alt="" />
+    </div>
+  );
+};
+
+export default Listing;
