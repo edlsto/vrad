1002cd5294f5624ab649c371efc352238b6ca762
Edward Stoner <edwardlstoner@gmail.com>
2020-03-25 20:15:01 -0600

    Add onClick for Area button

--- src/components/Area/Area.js
+++ src/components/Area/Area.js
@@ -1,18 +1,23 @@
 import React from "react";
 import "./Area.css";
 
-
-const Area = ({ about, id, shortName, longName, listings }) => {
-  return(
+const Area = ({ about, id, shortName, longName, chooseNeighborhood }) => {
+  return (
     <article id={id}>
       <div className="area-card-info">
         <h3>{shortName}</h3>
         <h4>{longName}</h4>
         <p>{about}</p>
       </div>
-      <button className="view-listings-button">View Listings</button>
+      <button
+        className="view-listings-button"
+        name={id}
+        onClick={e => chooseNeighborhood(e)}
+      >
+        View Listings
+      </button>
     </article>
-  )
-}
+  );
+};
 
-export default Area
+export default Area;
