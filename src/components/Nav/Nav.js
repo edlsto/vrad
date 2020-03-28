62f1070db3b366a164228289e6c322ffbde7b78d
Edward Stoner <edwardlstoner@gmail.com>
2020-03-28 15:41:09 -0600

    Add user greeting and favorites button

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,12 +1,21 @@
 import React from "react";
 import "./Nav.css";
+import { instanceOf } from "prop-types";
 
-const Nav = () => {
+const Nav = props => {
   return (
     <nav>
       <h1>
         VRAD<span>Vacation Rentals Around Denver</span>
       </h1>
+      {props.userinfo.name && (
+        <div className="greeting-btn-container">
+          <h2 className="greeting">Hello, {props.userinfo.name}</h2>
+          <button className="favorite-btn">
+            Favorites ({props.userinfo.favorites.length})
+          </button>
+        </div>
+      )}
     </nav>
   );
 };
