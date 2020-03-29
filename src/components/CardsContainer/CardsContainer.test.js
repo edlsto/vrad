c5d3c40fe22e6b49f30919b15ebc28c6e2ef8164
Dan Reardon <dreardon1021@gmail.com>
2020-03-29 11:35:56 -0600

    Fix CardContainer tests

--- src/components/CardsContainer/CardsContainer.test.js
+++ src/components/CardsContainer/CardsContainer.test.js
@@ -1,22 +1,23 @@
-import React from 'react'
+import React from 'react';
 import  { render, fireEvent } from '@testing-library/react';
 import CardsContainer from './CardsContainer.js';
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom';
+import { BrowserRouter as Router } from 'react-router-dom'
 
 describe('CardsContainer', () => {
   it('should render', () => {
-    const { getByTestId } = render(<CardsContainer areas={[]}/>)
+    const { getByTestId } = render(<Router><CardsContainer areas={[]}/></Router>)
 
     const container = getByTestId("card-container")
     expect(container).toBeInTheDocument()
   })
 
   it('should render a card', () => {
-    const { getByText } = render(<CardsContainer areas={[
+    const { getByText } = render(<Router><CardsContainer areas={[
       {area: 'short',
       id: 1,
       about:'About',
-      name: 'long',}]}/>)
+      name: 'long',}]}/></Router>)
 
     const areaDescriptionElement = getByText('About')
     const shortNameElement = getByText('short')
