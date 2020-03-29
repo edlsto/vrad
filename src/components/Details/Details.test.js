7189f19c1f627c71a5a68e292d1328bf4b09001f
Dan Reardon <dreardon1021@gmail.com>
2020-03-29 13:24:07 -0600

    Setup testing for App and Details

---
+++ src/components/Details/Details.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import  { render, fireEvent } from '@testing-library/react';
+import Details from './Details.js';
+import '@testing-library/jest-dom'
+
+describe("Details", () => {
+  it('should render what we expect', () => {
+    
+  })
+})
