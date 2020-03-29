7a7a0620b7881f70561387acf3e7785a49202756
Edward Stoner <edwardlstoner@gmail.com>
2020-03-29 07:07:03 -0600

    Refactor ListingsContainer

--- src/components/ListingsContainer/ListingsContainer.js
+++ src/components/ListingsContainer/ListingsContainer.js
@@ -9,27 +9,17 @@ class ListingsContainer extends Component {
   }
 
   render() {
-    let id;
-    if (this.props.location.pathname === "/areas/590") {
-      id = 590;
-    } else if (this.props.location.pathname === "/areas/751") {
-      id = 751;
-    } else if (this.props.location.pathname === "/areas/408") {
-      id = 408;
-    } else if (this.props.location.pathname === "/areas/240") {
-      id = 240;
-    }
-
-    let currentlyShownListings = this.props.listingsData.filter(
-      listing => listing.area_id === id
-    );
     return (
       <section className="listings-card-container">
         <h2 className="listings-header">Listings</h2>
         <div className="listings-container-inner">
-          {currentlyShownListings.map(listing => {
+          {this.props.listingsData.map(listing => {
             return (
-              <Listing name={listing.name} listing_id={listing.listing_id} />
+              <Listing
+                name={listing.name}
+                listing_id={listing.listing_id}
+                area_id={this.props.area_id}
+              />
             );
           })}
         </div>
