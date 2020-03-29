6c2b8ce1c14fa651a839be231abf3eeda6fadd2e
Edward Stoner <edwardlstoner@gmail.com>
2020-03-28 19:03:32 -0600

    Add classes

--- src/components/Area/Area.js
+++ src/components/Area/Area.js
@@ -12,9 +12,13 @@ const Area = ({ about, id, shortName, longName, chooseNeighborhood }) => {
   return (
     <article name="hi" style={sectionStyle} id={id}>
       <div className="area-card-info">
-        <h3 className="area-title">{shortName}</h3>
-        <h4 className="area-subtitle">{longName}</h4>
-        <p>{about}</p>
+        <h3 className="area-title">
+          <span className="area-highlight">{shortName}</span>
+        </h3>
+        <h4 className="area-subtitle">
+          <span className="area-highlight">{longName}</span>
+        </h4>
+        <p className="about">{about}</p>
       </div>
       <div name={id} className="view-listings-button-container">
         <NavLink to={"/areas/" + id} className="view-listings-button" name={id}>
