63c5f917e8ade7ff9c67a29dac2581b1c77f4d9c
Edward Stoner <edwardlstoner@gmail.com>
2020-03-29 09:35:31 -0600

    Add JSX for Details page

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -17,28 +17,74 @@ const Details = ({ selectedListing }) => {
     features
   );
 
-  let imageIds = ["a", "b", "c"];
-  let images = imageIds.map(image => {
-    return (
-      <img
-        src={"../../../images/" + listing_id + `_${image}.jpg`}
-        alt=""
-        className="listing-img"
-      />
-    );
-  });
+  const uppercase = string => {
+    let stringArray = string.split("");
+    stringArray[0] = stringArray[0].toUpperCase();
+    let newWord = stringArray.join("");
+    return newWord;
+  };
+
+  // let featureHTML = "";
+  // features.forEach(feature => {
+  //   featureHTML += `<li>${feature}</li`;
+  // });
+
+  // let imageIds = ["a", "b", "c"];
+  // let images = imageIds.map(image => {
+  //   return (
+  //     <img
+  //       src={"../../../images/" + listing_id + `_${image}.jpg`}
+  //       alt=""
+  //       className="listing-img"
+  //     />
+  //   );
+  // });
 
   return (
     <section className="details">
-      <div>{name}</div>
-      <div>
-        {street} {zip}
+      <div className="title-btn-container">
+        <div className="title-address-container">
+          <h1 className="details-title">{name}</h1>
+          <h2 className="details-address">
+            {street} {zip}
+          </h2>
+        </div>
+        <button className="details-fav-btn">
+          <i class="far fa-heart"></i>Favorite
+        </button>
       </div>
-      <div>Beds: {beds}</div>
-      <div>Baths: {baths}</div>
-      <div>Cost per night: {cost_per_night}</div>
-      <div>Features: {features}</div>
-      {images}
+
+      <div className="details-body">
+        <div className="other-details-container">
+          <div className="bed-bath-container">
+            <div className="bed-bath-display">
+              <div className="num-beds">{beds}</div> <div>beds</div>
+            </div>
+            <div className="bed-bath-display">
+              <div className="num-baths">{baths}</div> <div>baths</div>
+            </div>
+          </div>
+          <div className="cost-per-night-container">
+            <span className="cost-highlight">${cost_per_night}</span>/night
+          </div>
+
+          {!!features.length && (
+            <div className="features-list">
+              <h3 className="features-title">Features</h3>
+              <ul>
+                {features.map(feature => {
+                  return <li>{uppercase(feature)}</li>;
+                })}
+              </ul>
+            </div>
+          )}
+        </div>
+        <img
+          src={"../../../images/" + listing_id + `_A.jpg`}
+          className="details-img"
+          alt=""
+        />
+      </div>
     </section>
   );
 };
