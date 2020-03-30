e292f3fce70f5303985754a347e66df0c43e6888
Edward Stoner <edwardlstoner@gmail.com>
2020-03-30 13:53:00 -0600

    Add no favs message

--- src/components/ListingsContainer/ListingsContainer.js
+++ src/components/ListingsContainer/ListingsContainer.js
@@ -14,15 +14,30 @@ class ListingsContainer extends Component {
         data-testid="listing-container"
         className="listings-card-container"
       >
-        <h2 className="listings-header">{this.props.pathname.includes('/favorites') ? "Favorites" : "Listings"}</h2>
+        <h2 className="listings-header">
+          {this.props.pathname.includes("/favorites")
+            ? "Favorites"
+            : "Listings"}
+        </h2>
+        {this.props.favorites.length === 0 &&
+        this.props.pathname === "/favorites" ? (
+          <div class="no-favorites">
+            <h3>You have no favorites!</h3>
+          </div>
+        ) : (
+          ""
+        )}
         <div className="listings-container-inner">
           {this.props.listingsData.map(listing => {
+            console.log(this.props.favorites);
             return (
               <Listing
                 name={listing.name}
                 listing_id={listing.listing_id}
                 area_id={this.props.area_id}
                 key={listing.listing_id}
+                favorite={this.props.favorites.includes(listing.listing_id)}
+                addDeleteFavorite={this.props.addDeleteFavorite}
               />
             );
           })}
