682bca0636eafef2270333207496e24494da7756
Edward Stoner <edwardlstoner@gmail.com>
2020-04-02 16:42:51 -0600

    Get tests to pass again

--- src/components/CardsContainer/CardsContainer.test.js
+++ src/components/CardsContainer/CardsContainer.test.js
@@ -1,32 +1,38 @@
-import React from 'react';
-import  { render, fireEvent } from '@testing-library/react';
-import CardsContainer from './CardsContainer.js';
-import '@testing-library/jest-dom';
-import { BrowserRouter as Router } from 'react-router-dom'
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import CardsContainer from "./CardsContainer.js";
+import "@testing-library/jest-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
-describe('CardsContainer', () => {
-  it('should render', () => {
-    const { getByTestId } = render(<Router><CardsContainer areas={[]}/></Router>)
+describe("CardsContainer", () => {
+  it("should render", () => {
+    const { getByTestId } = render(
+      <Router>
+        <CardsContainer areas={[]} />
+      </Router>
+    );
 
-    const container = getByTestId("card-container")
-    expect(container).toBeInTheDocument()
-  })
+    const container = getByTestId("card-container");
+    expect(container).toBeInTheDocument();
+  });
 
-  it('should render a card', () => {
-    const { getByText } = render(<Router><CardsContainer areas={[
-      {area: 'short',
-      id: 1,
-      about:'About',
-      name: 'long',}]}/></Router>)
+  it("should render a card", () => {
+    const { getByText } = render(
+      <Router>
+        <CardsContainer
+          areas={[{ area: "short", id: 1, about: "About", name: "long" }]}
+        />
+      </Router>
+    );
 
-    const areaDescriptionElement = getByText('About')
-    const shortNameElement = getByText('short')
-    const longNameElement = getByText('long')
-    const buttonElement = getByText('View Listings')
+    const areaDescriptionElement = getByText("About");
+    const shortNameElement = getByText("short");
+    const longNameElement = getByText("(long)");
+    const buttonElement = getByText("View Listings");
 
-    expect(shortNameElement).toBeInTheDocument()
-    expect(longNameElement).toBeInTheDocument()
-    expect(areaDescriptionElement).toBeInTheDocument()
-    expect(buttonElement).toBeInTheDocument()
-  })
-})
+    expect(shortNameElement).toBeInTheDocument();
+    expect(longNameElement).toBeInTheDocument();
+    expect(areaDescriptionElement).toBeInTheDocument();
+    expect(buttonElement).toBeInTheDocument();
+  });
+});
