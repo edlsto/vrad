948beaa499bbd494a2a3d7a276f478229190bd5b
Edward Stoner <edwardlstoner@gmail.com>
2020-04-04 20:54:22 -0600

    Updates

---
+++ precache-manifest.2f5bc75b83f4831582ef624391013565.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c4d5a91e68feecaebdacd6efd6332cb",
+    "url": "/vrad/index.html"
+  },
+  {
+    "revision": "29368a61599d8c46a8c0",
+    "url": "/vrad/static/css/main.2c4bb882.chunk.css"
+  },
+  {
+    "revision": "9566a1de7bf538cf5b15",
+    "url": "/vrad/static/js/2.2ff1a358.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/vrad/static/js/2.2ff1a358.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29368a61599d8c46a8c0",
+    "url": "/vrad/static/js/main.fe466926.chunk.js"
+  },
+  {
+    "revision": "5a54e8f4795f5477f7c9",
+    "url": "/vrad/static/js/runtime-main.56b5c584.js"
+  },
+  {
+    "revision": "75626faed3d4dd38bb10ea60b6b2f4f9",
+    "url": "/vrad/static/media/denver-background.75626fae.jpg"
+  },
+  {
+    "revision": "ecdc2137f53fe306af66677152238a88",
+    "url": "/vrad/static/media/left.ecdc2137.png"
+  },
+  {
+    "revision": "1fb36206b72e31f651e2c5c8c24d4570",
+    "url": "/vrad/static/media/marker.1fb36206.png"
+  },
+  {
+    "revision": "203b031166a4478743b7526904f72f19",
+    "url": "/vrad/static/media/right.203b0311.png"
+  }
+]);
